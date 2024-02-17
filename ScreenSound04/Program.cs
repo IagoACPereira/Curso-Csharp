9937f2665a48171330c53e86bf27154c18f2ffd8
Iago Azevedo Costa Pereira <94400482+IagoACPereira@users.noreply.github.com>
2024-02-16 21:52:00 -0300

    Add files via upload

---
+++ ScreenSound04/Program.cs
@@ -0,0 +1,15 @@
+﻿using (HttpClient client = new()) 
+{
+    Console.WriteLine("Carregando...");
+    try
+    {
+        Console.Clear();
+        string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+
+        Console.WriteLine(resposta);
+    } catch (Exception)
+    {
+        Console.Clear();
+        Console.WriteLine($"Ocorreu um erro: Página não encontrada.");
+    }
+}
